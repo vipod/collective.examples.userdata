709cce35b6d3fe03b5d57331b34d106587b7f0aa
Vitaliy Podoba <vitaliypodoba@gmail.com>
2012-11-25 16:58:15 +0200

    add 100% tests coverage

---
+++ collective/examples/userdata/tests/test_doctests.py
@@ -0,0 +1,25 @@
+import doctest
+from unittest import TestSuite
+
+from Testing.ZopeTestCase import FunctionalDocFileSuite
+from Products.PloneTestCase.PloneTestCase import setupPloneSite, installPackage
+
+from plone.app.users.tests import TestCase
+
+installPackage('collective.examples.userdata')
+setupPloneSite(products=['collective.examples.userdata'])
+
+OPTIONFLAGS = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE |
+    doctest.REPORT_ONLY_FIRST_FAILURE)
+
+def test_suite():
+    tests = ['registration_forms.txt',
+             'userdata.txt',
+             ]
+    suite = TestSuite()
+    for test in tests:
+        suite.addTest(FunctionalDocFileSuite(test,
+            optionflags=OPTIONFLAGS,
+            package="collective.examples.userdata.tests",
+            test_class=TestCase))
+    return suite
